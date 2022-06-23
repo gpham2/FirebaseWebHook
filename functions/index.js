08d8b09d7c07067f912f29fc625d8eefb231c950
gpham2 <giangphamgia03@gmail.com>
2022-06-23 10:30:16 -0700

    refactoring done

--- functions/index.js
+++ functions/index.js
@@ -11,49 +11,69 @@ let transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
         user: 'giangphamgia03@gmail.com',
-        pass: 'lhidttsfivkvjcxv'
+        pass: 'bbenbcmoofhyrfiu'
     }
+    
 });
 
 /* Shipping Days */
 const shippingDays = 7;
 
 
-/* Webhook function */
-const helloWorld = (request, response) => {
-    
-    // Checking if email is within member database
-    const dest = request.query.dest
+/* Email Validation */ 
+function emailIsValid(destEmail, response) {
     let index = -1;
-    index = members.findIndex(items => items.email === dest.substring(0,dest.length-1));
-    if (index === -1) return response.send('Invalid member!');
+    index = members.findIndex(items => items.email === destEmail.substring(0,destEmail.length-1));
+    if (index === -1) response.send('Invalid member!');
+    return index;
+};
 
-    // Calculating Shipping arrival date
+/* Calculates Delivery Date */
+function getDeliveryDate(shipping) {
     var deliveryDate = new Date();
-    deliveryDate.setDate(deliveryDate.getDate() + shippingDays);
+    deliveryDate.setDate(deliveryDate.getDate() + shipping);
+    return deliveryDate;
+}
 
-    // Generating email message
-    const special = request.query.individualized;
-    const textPrompt = 'Hi ' + dest + '! The following items will be sent to you: ';
-    const textItems = special === "true" ? members[index].items.join(', ') : request.body.items.join(', ');
-    const textArrival = 'Your order will arrive on ' + deliveryDate.toLocaleDateString();
+
+/* Generates Email */
+function constructEmail(special, destEmail, defaultItems, dateObject, index) {
+    const textPrompt = 'Hi ' + destEmail + '! The following items will be sent to you: ';
+    const textItems = special === "true" ? members[index].items.join(', ') : defaultItems.join(', ');
+    const textArrival = 'Your order will arrive on ' + dateObject.toLocaleDateString();
     let options = {
         from: 'Giang Pham <giangphamgia03@gmail.com>',
-        to: dest,
+        to: destEmail,
         subject: 'Testing',
         text: textItems,
         html: '<div>' + textPrompt + '</div><b>' + textItems + '</b>' + '<div>' + textArrival + '</div>',
     };
+    return options;
+}
 
-    // Sending the email
-    return transporter.sendMail(options, (erro, info) => {
-        if(erro){
-            return response.send(erro.toString());
+
+/* Sends Email */
+async function sendEmail(options, response) {
+    return await transporter.sendMail(options, (error, info) => {
+        if(error){
+            return response.send(error.toString());
         }
         return response.send('Email Succesfully Sent');
     });
 }
 
 
-exports.helloWorld = functions.https.onRequest(helloWorld);
+/* Webhook function */
+
+async function notify(request, response) {
+
+    const dest = request.query.dest
+    let index = emailIsValid(dest, response);
+    if (index === -1) return;
+    let options = constructEmail(request.query.individualized, dest, request.body.items, getDeliveryDate(shippingDays), index);
+    return await sendEmail(options, response);
+}
+
+
+exports.notify = functions.https.onRequest(notify);
 
